2d4608d80f68bd82f1333eaa3bafd2491051b527
Zeke Sikelianos <zeke@sikelianos.com>
2019-08-02 13:52:04 -0700

    add more tests, rename some stuff, lint

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const assert = require('assert')
+const getSchema = require('./lib/get-schema')
+const getOperations = require('./lib/get-operations')
+const operate = require('./lib/operate')
 
 module.exports = function (opts = {}) {
   const defaults = {
@@ -6,12 +9,15 @@ module.exports = function (opts = {}) {
     key: null
   }
 
+  // attach options to the client object
   const client = Object.assign({}, defaults, opts)
-  assert(client.key, 'Missing required key.')
+  assert(client.key, 'Missing required option: `key`')
 
-  const schema = require('./lib/get-schema')(schemaVersion)
-  const operations = require('./lib/get-operations')(schema)
-  
+  // Load the requested schema version and generate a list of operations
+  const schema = getSchema(client.schemaVersion)
+  const operations = getOperations(schema)
+
+  // attach bound operations to the client object
   operations.forEach(operation => {
     client[operation.name] = operate.bind(operation)
   })
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,21 +1,33 @@
-// require dotenv 
 const { isPlainObject } = require('lodash')
-const client = require('..')(
-  { key: 'mykey' }
-)
+// const nock = require('nock')
 
-describe('crowdin client', () => {
-  test('it is an object', async () => {
+const Crowdin = require('..')
+const client = Crowdin({ key: 'mykey' })
+
+describe('module', () => {
+  test('exports a function for instantiating a client', () => {
+    expect(typeof Crowdin).toBe('function')
+  })
+})
+
+describe('client', () => {
+  test('is an object', async () => {
     expect(isPlainObject(client)).toBe(true)
   })
 
-  test('getProjectDetails', async () => {
+  test('requires an API key', () => {
+    expect(() => {
+      Crowdin()
+    }).toThrow('Missing required option: `key`')
+  })
+
+  test('attaches key to client object', async () => {
+    expect(typeof client.key).toBe('string')
+  })
+
+  test.skip('getProjectDetails', async () => {
     const details = await client.getProjectDetails('a_project')
     console.log(details)
     //   expect(details).toBe()
-  })
-
-  test('has key', async () => {
-    expect(typeof client.key).toBe('string')
   })
 })
