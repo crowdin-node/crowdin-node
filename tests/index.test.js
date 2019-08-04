4db7d94a16f5e5bb70243b1397e3144de900dc58
Zeke Sikelianos <zeke@sikelianos.com>
2019-08-04 00:11:36 -0700

    update docs

--- tests/index.test.js
+++ tests/index.test.js
@@ -29,21 +29,21 @@ describe('client', () => {
     expect(typeof client.config.key).toBe('string')
   })
 
-  test('client.api.project.branches.getMany() works', async () => {
+  test('client.project.branches.getMany() works', async () => {
     const mock = nock('https://api.crowdin.com')
       .get('/projects/myProjectId/branches')
       .query({ key: 'mykey' })
       .reply(200)
 
-    const result = await client.api.projects.branches.getMany('myProjectId')
+    const result = await client.projects.branches.getMany('myProjectId')
 
     expect(mock.isDone()).toBe(true)
     expect(result.statusCode).toBe(200)
   })
 
-  test.skip('client.api.projects.branches throws an error with invalid arguments', async () => {
+  test.skip('client.projects.branches.getMany() throws an error with invalid arguments', async () => {
     try {
-      await client.api.projects.branches.getMany()
+      await client.projects.branches.getMany()
     } catch (err) {
       expect(err).toEqual({
         error: 'Foobar'
